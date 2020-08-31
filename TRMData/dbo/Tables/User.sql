7b35eeff03f6f705a5cb48cf70dc36671f58b77d
Tiago <tiago@leadflash.com>
2020-08-31 16:48:05 -0400

    Register DB Tables
    The tables for the basic register system

---
+++ TRMData/dbo/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] Nvarchar(128) not null Primary key,
+	[FirstName] nvarchar(50) not null, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+
+)
