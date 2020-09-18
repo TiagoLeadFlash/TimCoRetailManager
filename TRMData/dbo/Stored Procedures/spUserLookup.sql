803ad17f095eede75592b059748fc5fcb2b64512
Tiago <tiago@leadflash.com>
2020-09-18 15:00:17 -0400

    added TRMData Library

---
+++ TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128)
+AS
+BEGIN
+	set nocount on;
+
+	SELECT id, FirstName, LastName, EmailAddress, CreateDate
+	FROM [dbo].[User]
+	WHERE Id = @Id
+
+END
---
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDataManager.Library.Internal.DataAccess
+
+{
+    public class SqlDataAccess
+    {
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public List<T> LoadData<T, U>(String storedprocedure, U parameters, string connectionsStringName)
+        {
+            string connectionsString = GetConnectionString(connectionsStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionsString))
+            {
+                List<T> rows = connection.Query<T>(storedprocedure, parameters, 
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(String storedprocedure, T parameters, string connectionsStringName)
+        {
+            string connectionsString = GetConnectionString(connectionsStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionsString))
+            {
+                connection.Execute(storedprocedure, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+    }
+}
