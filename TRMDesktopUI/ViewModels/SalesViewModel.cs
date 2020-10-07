95f8397341dfd24182bebcedac9f2b00ceb3828c
Tiago <tiago@leadflash.com>
2020-10-07 15:22:18 -0400

    Add dialog box and check permission
    Check to be user a user has access and alerts them if they do not

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -3,10 +3,12 @@ using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using TRMDataManager.Library.Models;
 using TRMDesktopUI.Library.Api;
 using TRMDesktopUI.Library.Helpers;
@@ -21,19 +23,45 @@ namespace TRMDesktopUI.ViewModels
         IConfigHelper _configHelper;
         ISaleEndpoint _saleEndPoint;
         IMapper _mapper;
+        private readonly StatusInfoViewModel _status;
+        private readonly IWindowManager _window;
+
         public SalesViewModel(IProductEndpoint productEndpoint, IConfigHelper configHelper, ISaleEndpoint saleEndPoint,
-            IMapper mapper)
+            IMapper mapper, StatusInfoViewModel status, IWindowManager window)
         {
             _productEndpoint = productEndpoint;
             _configHelper = configHelper;
             _saleEndPoint = saleEndPoint;
             _mapper = mapper;
+            _status = status;
+            _window = window;
         }
 
         protected override async void OnViewLoaded(object view)
         {
             base.OnViewLoaded(view);
-            await LoadProducts();
+            try
+            {
+                await LoadProducts();
+            }
+            catch (Exception ex)
+            {
+                dynamic setting = new ExpandoObject();
+                setting.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                setting.ResizeMode = ResizeMode.NoResize;
+                setting.Title = "System Error";
+
+                if (ex.Message == "Unauthorized")
+                {
+                    _status.UpdateMessage("Unauthorize Access", "You do not have permission to interact with the sales form");
+                    _window.ShowDialog(_status, null, setting);
+                }
+                else {
+                    _status.UpdateMessage("Fatal Exception", ex.Message);
+                    _window.ShowDialog(_status, null, setting);
+                }
+                TryClose();
+            }
             
         }
 
