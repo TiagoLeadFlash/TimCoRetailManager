c800a3202648498efedd70f75f76b521ce8929da
Tiago <tiago@leadflash.com>
2020-10-08 17:17:46 -0400

    added User View display
    Added api to get all users and a display view

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -54,6 +54,13 @@ namespace TRMDesktopUI.ViewModels
         {
             TryClose();
         }
+
+        public void UserManagement()
+        {
+            ActivateItem(IoC.Get<UserDisplayViewModel>());
+        }
+
+
         public void LogOut()
         {
             _user.ResetUserModel();
